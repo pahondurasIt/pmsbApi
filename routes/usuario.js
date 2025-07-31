dadb050212b88c37d23894d1786e2b821b923ea8
pleslysarahi <pleslysarahi@gmailcom>
2025-07-31 14:44:06 -0600

    Implement user management endpoints and refactor user routes for clarity

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/usuariosController');
+const userController = require("../controllers/usuariosController");
 
-router.get('/', userController.getUsuarios);
+router.get("/", userController.getUsuarios);
+router.post("/createuser", userController.createuser); // Ruta para crear un usuario
+router.get("/user-list", userController.getAllUsers);
 
 // Obtener permisos de los usuarios
-router.get('/permissions', userController.getPermissions);
-router.get('/permissionsByUser/:userID', userController.getPermissionById);
-router.post('/userProfile', userController.createProfileByUser);
+router.get("/permissions", userController.getPermissions);
+router.get("/permissionsByUser/:userID", userController.getPermissionById);
+router.post("/userProfile", userController.createProfileByUser);
 
 module.exports = router;
